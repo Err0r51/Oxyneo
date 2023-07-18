799e218538dd87f999a811815664c317e4aa1095
Err0r51 <f.junge@outlook.com>
2023-07-18 11:22:32 +0200

    feat(searchbar): :lipstick:  added first mock of search bar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,109 @@
 'use client'
 import {
   Box, Flex, Input,
-  List,
   ListItem,
-  ListIcon,
-  OrderedList,
+  Text,
   UnorderedList,
 } from '@chakra-ui/react'
-
-// absolut an searchbar
-// wenn results > 0 dann visible
+import React, { useState } from 'react'
 
 const styles = {
-  listReset: {
-    listStyleType: 'none',
-    padding: 0,
+  resultListWrapper: {
+    position: 'absolute',
+    top: '100%',
+    left: 0,
+    width: '100%',
+    zIndex: 1,
+    borderRadius: '4px',
+    overflow: 'hidden',
   },
+  resultList: {
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    padding: '0.5rem',
+  },
+}
 
+interface IStock {
+  name: string
+  symbol: string
+  description: string
+}
+
+interface IStockDictionary {
+  [id: string]: IStock
+}
+
+const stockList: IStockDictionary = {
+  AAPL: { name: 'Apple', symbol: 'AAPL', description: 'Apple Inc.' },
+  MSFT: { name: 'Microsoft', symbol: 'MSFT', description: 'Microsoft Corporation' },
+  AMZN: { name: 'Amazon', symbol: 'AMZN', description: 'Amazon.com, Inc.' },
+  GOOG: { name: 'Alphabet', symbol: 'GOOG', description: 'Alphabet Inc.' },
+  FB: { name: 'Facebook', symbol: 'FB', description: 'Facebook, Inc.' },
+  TSLA: { name: 'Tesla', symbol: 'TSLA', description: 'Tesla, Inc.' },
 }
 
 export default function Home() {
+  const [searchEntry, setSearchEntry] = useState('')
+
+  // 👇️ type as React.ChangeEvent<HTMLInputElement>
+  // or React.ChangeEvent<HTMLTextAreaElement> (for textarea elements)
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchEntry(event.target.value)
+    console.log(event.target.value)
+  }
+
+  // Filter the stockList based on the searchEntry
+  const filteredStocks = Object.keys(stockList).filter(
+    key =>
+      stockList[key].name.toLowerCase().includes(searchEntry.toLowerCase())
+      || stockList[key].symbol.toLowerCase().includes(searchEntry.toLowerCase()),
+  )
+
   return (
     <main>
       <Box p="12">
-        <Input placeholder='Basic usage' size='lg' />
-        <UnorderedList style={styles.listReset}>
-          <ListItem>Lorem ipsum dolor sit amet</ListItem>
-          <ListItem>Consectetur adipiscing elit</ListItem>
-          <ListItem>Integer molestie lorem at massa</ListItem>
-          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-        </UnorderedList>
+        <Flex position="relative">
+          <Input
+            placeholder="Enter the stock name or symbol"
+            size="lg"
+            onInput={handleChange}
+            value={searchEntry}
+          />
+          {searchEntry.length > 0 && (
+            <Box
+              position="absolute"
+              top="100%"
+              left="0"
+              width="100%"
+              zIndex="1"
+              borderRadius="4px"
+              boxShadow="0 2px 4px rgba(0, 0, 0, 0.1)"
+              mt="0.5rem"
+              p="0.5rem"
+              bg="gray.800"
+            >
+              <UnorderedList listStyleType="none" m={0} p={0}>
+                {filteredStocks.slice(0, 6).map(key => (
+                  <ListItem
+                    key={key}
+                    _hover={{ backgroundColor: 'gray.700', cursor: 'pointer', borderRadius: '4px' }}
+                  >
+                    <Flex align="center" justify="space-between">
+                      <Box>
+                        <Text fontWeight="bold">{stockList[key].name}</Text>
+                        <Text>{stockList[key].symbol}</Text>
+                      </Box>
+                      <Box textAlign="right">
+                        <Text>{stockList[key].description}</Text>
+                      </Box>
+                    </Flex>
+                  </ListItem>
+                ))}
+              </UnorderedList>
+            </Box>
+          )}
+        </Flex>
       </Box>
-
     </main>
   )
-}
+};
