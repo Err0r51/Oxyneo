e76355e80226339064d5a6641fd09e6a55ff8699
Err0r51 <f.junge@outlook.com>
2023-08-01 14:08:37 +0200

    feat(PortfolioCalculation): :construction:  added a PoC implementation of Portfolio Calc in GCP functions

--- api/microservices/StockPortfolioBalance/main.py
+++ api/microservices/StockPortfolioBalance/main.py
@@ -1,16 +1,13 @@
 import functions_framework
 from datetime import datetime
 import logging
+import os
 import requests
-
-# from ..helpers import json_response
-
-from typing import List, Dict, Any
-from datetime import datetime, date
+from typing import Dict, Any
+from concurrent.futures import ThreadPoolExecutor
 
 from flask import jsonify
 import pandas as pd
-
 from pypfopt.expected_returns import mean_historical_return
 from pypfopt.risk_models import CovarianceShrinkage
 from pypfopt.efficient_frontier import EfficientFrontier
@@ -19,7 +16,8 @@ from pypfopt import objective_functions
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-API_KEY = ""
+API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
+logging.info("ALPHAVANTAGE_API_KEY: %s", API_KEY)
 
 
 @functions_framework.http
@@ -32,10 +30,6 @@ def getPortfolioCalculation(request):
         The response text, or any set of values that can be turned into a
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    Note:
-        For more information on how Flask integrates with Cloud
-        Functions, see the `Writing HTTP functions` page.
-        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
     """
     logging.info("getPortfolioCalculation function processed a request")
 
@@ -57,15 +51,17 @@ def getPortfolioCalculation(request):
             stock_dict[symbol] = stock_data
 
     # Create a DataFrame by extracting closing prices for each symbol
-    df_list = [extractClosingPrices(stock_dict[symbol], symbol) for symbol in symbols]
+    df_list = [
+        extractClosingPrices(stock_dict[symbol], symbol) for symbol in symbols
+    ]
 
     # Merge individual DataFrames into a single DataFrame with rows as dates and columns as symbols
     stock_df = pd.concat(df_list, axis=1)
-    stock_df.index.name = 'date'  # Optional: Set the index name
+    stock_df.index.name = "date"  # Optional: Set the index name
 
-    logging.info("result_df: %s", stock_df)
+    portfolio_object = calculatePortfolioWeightsAsync(stock_df)
 
-    return jsonify(stock_df.to_json(orient='index'))  # Placeholder response for now
+    return jsonify(portfolio_object)  # Placeholder response for now
 
 
 def getStockTickerData(symbol: str) -> Dict[str, Any]:
@@ -79,18 +75,48 @@ def getStockTickerData(symbol: str) -> Dict[str, Any]:
 
     response = requests.get(base_url, params=params)
     logging.info("Alphavantage Api response code: %s", response.status_code)
-    logging.info("Alphavantage Api response: %s", response.json())
 
     # TODO: Handle other status codes
     if (
-        response.status_code
-        == 200 and response.json().get("Error Message")
-        is None
+        response.status_code == 200
+        and response.json().get("Error Message") is None
     ):
         return response.json()
     else:
         logging.error("Alphavantage Api response error: %s", response.json())
         return False
+
+
+def calculatePortfolioWeights(stock_df: pd.DataFrame) -> Dict[str, Dict]:
+    mean_hist_return = mean_historical_return(stock_df)
+    estimated_covar_matrix = CovarianceShrinkage(stock_df).ledoit_wolf()
+    efficient_frontier = EfficientFrontier(
+        mean_hist_return, estimated_covar_matrix
+    )
+
+    # Here we inject a L2 Regularisation so not so many stocks are returned
+    # with 0% distribution
+    # TODO: Adjust accordingly
+    efficient_frontier.add_objective(objective_functions.L2_reg, gamma=0.1)
+    weights = efficient_frontier.max_sharpe()
+    clean_weights = efficient_frontier.clean_weights()
+
+    logging.info("clean_weights: %s", clean_weights)
+
+    return {
+        "weights": clean_weights,
+        "expected_performance": "not implemented yet",
+    }
+
+
+def calculatePortfolioWeightsAsync(
+    stock_df: pd.DataFrame,
+) -> Dict[str, Dict]:
+    # Use ThreadPoolExecutor to parallelize the calculation
+    with ThreadPoolExecutor() as executor:
+        # Submit the calculation to the thread pool
+        future = executor.submit(calculatePortfolioWeights, stock_df)
+        return future.result()
 
 
 def extractClosingPrices(data: dict, symbol: str) -> pd.DataFrame:
@@ -147,7 +173,7 @@ def handleUpStreamApiError(e):
         jsonify(
             isError=True,
             data={
-                "message": "Error: encountered an error while calling upstream api, tru again later"
+                "message": "Error: encountered an error while calling upstream api, try again later"
             },
             statusCode=500,
         ),
