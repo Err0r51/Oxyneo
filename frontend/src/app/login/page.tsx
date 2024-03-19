aa0ef7f6945b919207bf6124fdf6e398c169efda
Err0r51 <f.junge@outlook.com>
2024-03-19 14:50:25 +0100

    feat(auth): :sparkles:  added working signup and signin process

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,79 +1,69 @@
-
 "use client"
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { redirect, useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import Image from 'next/image';
+import cover from '/public/cover.jpg';
 
+import { useState } from 'react';
+import { revalidatePath } from 'next/cache';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import Image from 'next/image'
-import cover from '/public/cover.jpg'
-import { login, signup } from './actions'
-
-
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-
-
-import { Input } from "@/components/ui/input"
-
-const FormSchema = z.object({
-  email: z.string().email({
-    message: "Invalid email address.",
-  }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters long.",
-  }),
-})
+const LoginSchema = z.object({
+  email: z.string().email("Invalid email address."),
+  password: z.string().min(8, "Password is missing or too short."),
+});
 
 export default function LoginPage() {
   const router = useRouter();
   const supabase = createClientComponentClient();
-  const { control, handleSubmit, formState: { errors } } = useForm({
-    resolver: zodResolver(FormSchema),
-  });
+  const [formError, setFormError] = useState("");
 
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+  const { ...form } = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
-  })
+  });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data)
+  const handleSignIn = async (data: z.infer<typeof LoginSchema>) => {
+    const { email, password } = data;
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+      console.error(error.message);
+      setFormError(`Error: ${error.message}`);
+    } else {
+      revalidatePath('/', 'layout')
+      router.refresh();
+    }
+  };
+
+  const handleSignUp = async (data: z.infer<typeof LoginSchema>) => {
+    const { email, password } = data;
+    const { error } = await supabase.auth.signUp({
+      email, password
+    });
+    if (error) {
+      console.error(error.message);
+      setFormError(`Error: ${error.message}`);
+    } else {
+      router.push("/signup-success");
+    }
   }
 
-  const handleSignIn = async () => {
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-    if (error)  
-      redirect('/error')
-    
-    router.refresh()
-  }
+  return (
 
-
-return (
-  <div className="flex flex-col md:flex-row h-screen">
-    <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
-      <div>
-        <h2 className="text-3xl font-bold mb-4">Login</h2>
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold mb-4">Login</h1>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <form onSubmit={form.handleSubmit(handleSignIn)} className="space-y-6">
             <FormField
               control={form.control}
               name="email"
@@ -81,7 +71,7 @@ return (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input placeholder="student@berkeleyfans.com" {...field} />
+                    <Input placeholder="shadcn" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -92,29 +82,27 @@ return (
               name="password"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input type="password" placeholder="**********" {...field} />
+                    <Input type="password" placeholder="********" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
             <div className="flex justify-between">
-              <Button variant="outline" formAction={signup}>Sign up</Button>
-              <Button type="submit" formAction={login}>Login</Button>
+              <Button type="button" variant="outline" onClick={() => form.handleSubmit(handleSignUp)()}>Sign up</Button>
+              <Button type="submit">Login</Button>
             </div>
+            {formError && (
+              <div className="text-red-500">{formError}</div>
+            )}
           </form>
         </Form>
       </div>
+      <div className="hidden md:block md:w-2/3">
+        <Image src={cover} alt="Cover image" className="h-full w-full object-cover" />
+      </div>
     </div>
-    <div className="hidden md:block md:w-2/3">
-      <Image
-        src={cover}
-        alt="Photo by Verne Ho on Unsplash"
-        className="h-full w-full object-cover"
-      />
-    </div>
-  </div>
-)
+  );
 }
---
+++ frontend/src/app/signup-success/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MailCheck } from 'lucide-react';
+
+
+const SignupSuccessPage: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <MailCheck size={100} className="mb-4" />
+            <h1 className="text-2xl font-bold mb-2">Signup Successful!</h1>
+            <p>Please check your email to verify your account.</p>
+        </div>
+    );
+};
+
+export default SignupSuccessPage;
